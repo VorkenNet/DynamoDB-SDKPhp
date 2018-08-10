8243f893a3b3be57594cc9ca8679b0a2b45c4179
Davide Valsecchi <40401457+VorkenNet@users.noreply.github.com>
2018-08-10 15:50:18 +0200

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+// Per generare le credenziali di accesso:
+// AWS->Services->IAM->Users-> YouUser-> SecurityCredentials -> AccessKey
+$key="<yourAccesskeyID "; //"Access key ID"
+$secret="<SecretAccessKey>"; //"Secret access key"
+$region="<yourRegion>"; // Esemple: 'eu-central-1';
+?>
